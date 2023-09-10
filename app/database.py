7db2471ff2a7d3767702273dafc11fb2be466ba7
Nabil Merchant <merchant.nabil@gmail.com>
2023-09-09 20:32:21 -0500

    Creating users and refactoring files

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:<some-password>@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:<Something-here>@localhost/fastapi"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
